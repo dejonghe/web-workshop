fa378671323a791b6e5852270928377309421a8c
Derek DeJonghe <derek.dejonghe@rightbrainnetworks.com>
2020-01-21 18:19:16 -0500

    running http server

--- src/main.go
+++ src/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-    "io"
-    "log"
-    "net/http"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
+
+type point struct {
+    x, y int
+}
+
 func main() {
-    helloHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, "Hello, World!\n")
-    }
-    http.HandleFunc("/hello", helloHandler)
-    log.Fatal(http.ListenAndServe(":8000", nil))
+	html := `<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Example</title>
+        </head>
+        <body>
+            <p>This is an example of a simple HTML page with one paragraph.</p>
+            <p>Our addtion value is %f.</p>
+            <p>Our addtion type is %T.</p>
+            <p>Our point value is %v.</p>
+            <p>Our point type is %T.</p>
+        </body>
+    </html>`
+	addition := 2 + 2.2
+
+    p := point{1,2}
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, fmt.Sprintf(html, addition, addition, p, p))
+	}
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
